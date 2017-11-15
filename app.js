20feb660d2ae15203a5c588288b7f0c0e577ddca
Jingze Ma <majingze0508@gmail.com>
2017-11-15 00:21:27 -0500

    build node.js server

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'pug');
+
+app.use(express.static(__dirname + '/public');
+
+app.get('/', function(req, res){
+	res.render('login');
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Server start');
+}
